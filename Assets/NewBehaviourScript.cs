2117504b1392b7f3bd93a7d16ececd7324e4485d
Mike Wise <mwise@microsoft.com>
2019-06-13 15:11:56 -0700

    first

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class UxSetting<T>
+{
+    [SerializeField]
+    public T val;
+}
+    public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public UxSetting<bool> setting;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
