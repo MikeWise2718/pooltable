8790888bd0a7c235f546dd82e315ea2d8d89858e
Mike Wise <mwise@microsoft.com>
2019-06-13 15:37:37 -0700

    Signed-off-by: Mike Wise <mwise@microsoft.com>

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,12 +7,28 @@ public class UxSetting<T>
 {
     [SerializeField]
     public T val;
+    [SerializeField]
+    public bool bval;
 }
-    public class NewBehaviourScript : MonoBehaviour
+
+[System.Serializable]
+public class UxSetting1:UxSetting<bool>
+{
+
+}
+
+[System.Serializable]
+public class UxSetting0
+{
+    [SerializeField]
+    public bool val;
+}
+public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    public UxSetting0 setting0;
+    public UxSetting1 setting1;
     public UxSetting<bool> setting;
-
     void Start()
     {
         
